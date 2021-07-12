f6807a5c1f19005e07e242e6c1fd8f356f581452
mahaalj <87118715+mahaalj@users.noreply.github.com>
2021-07-12 21:04:59 +0300

    Update Hangman.py
    code excluding hangman image

--- Hangman.py
+++ Hangman.py
@@ -4,4 +4,62 @@
 
 import random
 from words import words
-print(words)
+
+number_mistake = 0
+letters_guessed = []
+trials = len(hangman_image)
+word = random.choice(words)
+word_letters = list(word)
+wrong_letters = []
+
+print ()
+print ('Welcome to Hangman! your word has {} letters'.format(len(word_letters)))
+
+while number_mistake < trials:
+    print()
+    print('Wrong letters: ', end='')
+    for letter in wrong_letters:
+        print('{}, '.format(letter), end='')
+    print()
+    print('You have {} many guesses left'.format(trials - number_mistake))
+    player_letter = input ('Please enter a letter: ')
+
+
+    while player_letter in letters_guessed or player_letter in wrong_letters:
+        print()
+        print('This letter has already been guessed, Try another letter')
+        player_letter = input('Please enter a letter: ')
+
+    if player_letter not in word_letters:
+        number_mistake += 1
+        wrong_letters.append(player_letter)
+
+    print()
+    print('Word: ', end='')
+
+    for letter in word_letters:
+        if player_letter == letter:
+            letters_guessed.append(player_letter)
+
+    for letter in word_letters:
+        if letter in letters_guessed:
+            print(letter + ' ', end='')
+        else:
+            print('_ ', end='')
+
+    print()
+    if number_mistake:
+        print(hangman_image[number_mistake -1])
+        print()
+
+
+    if len(letters_guessed) == len(word_letters):
+        print()
+        print('Congratulations, You won !')
+        break
+
+if number_mistake == trials:
+    print()
+    print('Unfortunately, You Have Lost, Try Again!')
+
+
